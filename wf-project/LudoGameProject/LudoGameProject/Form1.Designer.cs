a5d580469a35302cd864809326e000b657f08ad4
Harrilymarrily <hpjshpjs@gmail.com>
2024-12-15 20:28:05 +0000

    Add files via upload

---
+++ wf-project/LudoGameProject/LudoGameProject/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace LudoGameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Diceresult = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.bluePawn = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.redPawn = new System.Windows.Forms.PictureBox();
+            this.greenPawn = new System.Windows.Forms.PictureBox();
+            this.yellowPawn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePawn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPawn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenPawn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowPawn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1074, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Diceresult
+            // 
+            this.Diceresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Diceresult.Location = new System.Drawing.Point(1074, 213);
+            this.Diceresult.Multiline = true;
+            this.Diceresult.Name = "Diceresult";
+            this.Diceresult.Size = new System.Drawing.Size(75, 124);
+            this.Diceresult.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(801, 417);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 63);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(545, 801);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 65);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(160, 546);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(63, 61);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(417, 161);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(62, 62);
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // bluePawn
+            // 
+            this.bluePawn.BackColor = System.Drawing.Color.Transparent;
+            this.bluePawn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bluePawn.BackgroundImage")));
+            this.bluePawn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bluePawn.Location = new System.Drawing.Point(864, 542);
+            this.bluePawn.Name = "bluePawn";
+            this.bluePawn.Size = new System.Drawing.Size(65, 65);
+            this.bluePawn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bluePawn.TabIndex = 9;
+            this.bluePawn.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(1074, 477);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 11;
+            this.start.Text = "Start Game";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1074, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "You are Blue";
+            // 
+            // redPawn
+            // 
+            this.redPawn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redPawn.BackgroundImage")));
+            this.redPawn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redPawn.Location = new System.Drawing.Point(414, 864);
+            this.redPawn.Name = "redPawn";
+            this.redPawn.Size = new System.Drawing.Size(65, 65);
+            this.redPawn.TabIndex = 13;
+            this.redPawn.TabStop = false;
+            // 
+            // greenPawn
+            // 
+            this.greenPawn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("greenPawn.BackgroundImage")));
+            this.greenPawn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.greenPawn.Location = new System.Drawing.Point(94, 417);
+            this.greenPawn.Name = "greenPawn";
+            this.greenPawn.Size = new System.Drawing.Size(65, 65);
+            this.greenPawn.TabIndex = 14;
+            this.greenPawn.TabStop = false;
+            // 
+            // yellowPawn
+            // 
+            this.yellowPawn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("yellowPawn.BackgroundImage")));
+            this.yellowPawn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.yellowPawn.Location = new System.Drawing.Point(544, 95);
+            this.yellowPawn.Name = "yellowPawn";
+            this.yellowPawn.Size = new System.Drawing.Size(65, 65);
+            this.yellowPawn.TabIndex = 15;
+            this.yellowPawn.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1213, 749);
+            this.Controls.Add(this.yellowPawn);
+            this.Controls.Add(this.greenPawn);
+            this.Controls.Add(this.redPawn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.bluePawn);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Diceresult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePawn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redPawn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenPawn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowPawn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Diceresult;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox bluePawn;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox redPawn;
+        private System.Windows.Forms.PictureBox greenPawn;
+        private System.Windows.Forms.PictureBox yellowPawn;
+    }
+}
+
---
+++ wf-project/LudoGameProject/LudoGameProject/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LudoGameProject
+{
+    public partial class Form1 : Form
+    {
+        Random Number = new Random();
+        List<BoardDirection> BlueDirections = new List<BoardDirection>();
+        List<BoardDirection> RedDirections = new List<BoardDirection>();
+        List<BoardDirection> GreenDirections = new List<BoardDirection>();
+        List<BoardDirection> YellowDirections = new List<BoardDirection>();
+
+        int bluePiecePos = 0;
+        int bluePieceDirection = 0;
+
+        int redPiecePos = 0;
+        int redPieceDirection = 0;
+
+        int greenPiecePos = 0;
+        int greenPieceDirection = 0;
+
+        int yellowPiecePos = 0;
+        int yellowPieceDirection = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            BlueDirections.Add(new BoardDirection(5, "LAcross"));    //Direction and turns for Pawns, the number is the amount of places, the word determines which direction.
+            BlueDirections.Add(new BoardDirection(6, "Down"));
+            BlueDirections.Add(new BoardDirection(2, "LAcross"));
+            BlueDirections.Add(new BoardDirection(6, "Up"));
+            BlueDirections.Add(new BoardDirection(6, "LAcross"));
+            BlueDirections.Add(new BoardDirection(2, "Up"));
+            BlueDirections.Add(new BoardDirection(6, "RAcross"));
+            BlueDirections.Add(new BoardDirection(6, "Up"));
+            BlueDirections.Add(new BoardDirection(2, "RAcross"));
+            BlueDirections.Add(new BoardDirection(6, "Down"));
+            BlueDirections.Add(new BoardDirection(6, "RAcross"));
+            BlueDirections.Add(new BoardDirection(1, "Down"));
+            BlueDirections.Add(new BoardDirection(6, "LAcross"));
+
+            RedDirections.Add(new BoardDirection(5, "Up"));
+            RedDirections.Add(new BoardDirection(6, "LAcross"));
+            RedDirections.Add(new BoardDirection(2, "Up"));
+            RedDirections.Add(new BoardDirection(6, "RAcross"));
+            RedDirections.Add(new BoardDirection(6, "Up"));
+            RedDirections.Add(new BoardDirection(2, "RAcross"));
+            RedDirections.Add(new BoardDirection(6, "Down"));
+            RedDirections.Add(new BoardDirection(6, "RAcross"));
+            RedDirections.Add(new BoardDirection(2, "Down"));
+            RedDirections.Add(new BoardDirection(6, "LAcross"));
+            RedDirections.Add(new BoardDirection(6, "Down"));
+            RedDirections.Add(new BoardDirection(1, "LAcross"));
+            RedDirections.Add(new BoardDirection(6, "Up"));
+
+            GreenDirections.Add(new BoardDirection(5, "RAcross"));
+            GreenDirections.Add(new BoardDirection(6, "Up"));
+            GreenDirections.Add(new BoardDirection(2, "RAcross"));
+            GreenDirections.Add(new BoardDirection(6, "Down"));
+            GreenDirections.Add(new BoardDirection(6, "RAcross"));
+            GreenDirections.Add(new BoardDirection(2, "Down"));
+            GreenDirections.Add(new BoardDirection(6, "LAcross"));
+            GreenDirections.Add(new BoardDirection(6, "Down"));
+            GreenDirections.Add(new BoardDirection(2, "LAcross"));
+            GreenDirections.Add(new BoardDirection(6, "Up"));
+            GreenDirections.Add(new BoardDirection(6, "LAcross"));
+            GreenDirections.Add(new BoardDirection(1, "Up"));
+            GreenDirections.Add(new BoardDirection(6, "RAcross"));
+
+            YellowDirections.Add(new BoardDirection(5, "Down"));
+            YellowDirections.Add(new BoardDirection(6, "RAcross"));
+            YellowDirections.Add(new BoardDirection(2, "Down"));
+            YellowDirections.Add(new BoardDirection(6, "LAcross"));
+            YellowDirections.Add(new BoardDirection(6, "Down"));
+            YellowDirections.Add(new BoardDirection(2, "LAcross"));
+            YellowDirections.Add(new BoardDirection(6, "Up"));
+            YellowDirections.Add(new BoardDirection(6, "LAcross"));
+            YellowDirections.Add(new BoardDirection(2, "Up"));
+            YellowDirections.Add(new BoardDirection(6, "RAcross"));
+            YellowDirections.Add(new BoardDirection(6, "Up"));
+            YellowDirections.Add(new BoardDirection(1, "RAcross"));
+            YellowDirections.Add(new BoardDirection(6, "Down"));
+        }
+        public void playerturn()
+        {
+            List<string> players = new List<string>();
+
+            players.Add("blue");
+            players.Add("red");
+            players.Add("green");
+            players.Add("yellow");
+
+
+        }
+        private void start_Click(object sender, EventArgs e)
+        {           
+            start.Text = "Your turn";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int a = Number.Next(1, 7);      // Replicates the dice roll for blue
+            Diceresult.Text = a.ToString();
+
+            int b = Number.Next(1, 7);      // Replicates the dice roll for red
+            Diceresult.Text = b.ToString();
+
+            int c = Number.Next(1, 7);      // Replicates the dice roll for green
+            Diceresult.Text = c.ToString();
+
+            int d = Number.Next(1, 7);      // Replicates the dice roll for yellow
+            Diceresult.Text = d.ToString();
+
+            MoveBluepiece(a);
+            MoveRedpiece(b);       
+            MoveGreenpiece(c);
+            MoveYellowpiece(d);
+
+        }
+
+        void MoveBluepiece(int distance)
+        {
+            BoardDirection current = BlueDirections[bluePieceDirection];
+            for(int i=0;i< distance; i++)
+            {
+                if (bluePiecePos != current.amount)
+                {
+                    switch(current.direction)  //Determines how the Pawn moves.
+                    {
+                        case "LAcross":
+                            bluePawn.Location = new Point(bluePawn.Location.X - 64, bluePawn.Location.Y);   // Moves the pawn on the X-Axis
+                            break;
+                        case "RAcross":
+                            bluePawn.Location = new Point(bluePawn.Location.X + 64, bluePawn.Location.Y);
+                            break;
+                        case "Down":
+                            bluePawn.Location = new Point(bluePawn.Location.X, bluePawn.Location.Y+65);   // Moves the pawn on the Y-Axis
+                            break;
+                        case "Up":
+                            bluePawn.Location = new Point(bluePawn.Location.X, bluePawn.Location.Y-65);
+                            break;
+                    }
+                    bluePiecePos++;
+                }
+                else
+                {
+                    bluePieceDirection++;
+                    bluePiecePos = 0;
+                    current = BlueDirections[bluePieceDirection];
+                }
+            }
+        }
+
+        void MoveRedpiece(int distance)
+        {
+            BoardDirection current = RedDirections[redPieceDirection];
+            for (int i = 0; i < distance; i++)
+            {
+                if (redPiecePos != current.amount)
+                {
+                    switch (current.direction)
+                    {
+                        case "LAcross":
+                            redPawn.Location = new Point(redPawn.Location.X - 64, redPawn.Location.Y);
+                            break;
+                        case "RAcross":
+                            redPawn.Location = new Point(redPawn.Location.X + 64, redPawn.Location.Y);
+                            break;
+                        case "Down":
+                            redPawn.Location = new Point(redPawn.Location.X, redPawn.Location.Y + 65);
+                            break;
+                        case "Up":
+                            redPawn.Location = new Point(redPawn.Location.X, redPawn.Location.Y - 65);
+                            break;
+                    }
+                    redPiecePos++;
+                }
+                else
+                {
+                    redPieceDirection++;
+                    redPiecePos = 0;
+                    current = RedDirections[redPieceDirection];
+                }
+            }
+        }
+        void MoveGreenpiece(int distance)
+        {
+            BoardDirection current = GreenDirections[greenPieceDirection];
+            for (int i = 0; i < distance; i++)
+            {
+                if (greenPiecePos != current.amount)
+                {
+                    switch (current.direction)
+                    {
+                        case "LAcross":
+                            greenPawn.Location = new Point(greenPawn.Location.X - 65, greenPawn.Location.Y);
+                            break;
+                        case "RAcross":
+                            greenPawn.Location = new Point(greenPawn.Location.X + 65, greenPawn.Location.Y);
+                            break;
+                        case "Down":
+                            greenPawn.Location = new Point(greenPawn.Location.X, greenPawn.Location.Y + 65);
+                            break;
+                        case "Up":
+                            greenPawn.Location = new Point(greenPawn.Location.X, greenPawn.Location.Y - 65);
+                            break;
+                    }
+                    greenPiecePos++;
+                }
+                else
+                {
+                    greenPieceDirection++;
+                    greenPiecePos = 0;
+                    current = GreenDirections[greenPieceDirection];
+                }
+            }
+        }
+        void MoveYellowpiece(int distance)
+        {
+            BoardDirection current = YellowDirections[yellowPieceDirection];
+            for (int i = 0; i < distance; i++)
+            {
+                if (yellowPiecePos != current.amount)
+                {
+                    switch (current.direction)
+                    {
+                        case "LAcross":
+                            yellowPawn.Location = new Point(yellowPawn.Location.X - 65, yellowPawn.Location.Y);
+                            break;
+                        case "RAcross":
+                            yellowPawn.Location = new Point(yellowPawn.Location.X + 65, yellowPawn.Location.Y);
+                            break;
+                        case "Down":
+                            yellowPawn.Location = new Point(yellowPawn.Location.X, yellowPawn.Location.Y + 65);
+                            break;
+                        case "Up":
+                            yellowPawn.Location = new Point(yellowPawn.Location.X, yellowPawn.Location.Y - 65);
+                            break;
+                    }
+                    yellowPiecePos++;
+                }
+                else
+                {
+                    yellowPieceDirection++;
+                    yellowPiecePos = 0;
+                    current = YellowDirections[yellowPieceDirection];
+                }
+            }
+        }
+    }
+
+    public class BoardDirection
+    {
+        public int amount;
+        public string direction;
+
+        public BoardDirection(int a, string d)   // class for Pawn movement and direction
+        {
+            amount = a;
+            direction = d;
+        }
+    }
+}
